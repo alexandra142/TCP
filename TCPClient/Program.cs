68465203a11f2701c52960480129e749f4171d94
Alexandra Pistrakova <alex.pistrakova@gmail.com>
2017-05-18 13:03:13 +0200

    Add project files.

---
+++ TCPClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try // 1
+            {
+                s.Connect(IPAddress.Parse("127.0.0.1"), 6666); // 2
+                while (true)
+                {
+                    Communicate(s);
+                }
+            }
+            catch(Exception ex)
+            {
+                // nepripojeno
+            }
+        }
+
+        private static void Communicate(Socket socket)
+        {
+            Console.Write("Zadej nejakej text : ");
+            string q = Console.ReadLine();                 // 3
+            byte[] data = Encoding.Default.GetBytes(q);    // 3
+            socket.Send(data);
+        }
+    }
+}
---
+++ TCPServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static byte[] data;  // 1
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // 2
+            socket.Bind(new IPEndPoint(IPAddress.Any, 6666)); // 3
+            socket.Listen(10); // 4
+
+            while (true)
+            {
+                Socket accepteddata = socket.Accept(); // 5
+                data = new byte[accepteddata.SendBufferSize]; // 6
+                Listen(accepteddata);
+            }
+        }
+
+        private static void Listen(Socket accepteddata)
+        {
+            int j = accepteddata.Receive(data); // 7
+            byte[] adata = new byte[j];         // 7
+            for (int i = 0; i < j; i++)         // 7
+                adata[i] = data[i];             // 7
+            string dat = Encoding.Default.GetString(adata); // 8
+            Console.WriteLine(dat);
+        }
+    }
+}
